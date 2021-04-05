ea006280feeeec23d9e7f89b7ca8f20faf880740
Rodrigo Manozzo <rodrigomanozzo@gmail.com>
2021-04-05 18:14:04 -0300

    creating calculator

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operations]");
+const delete = document.querySelector("[data-delete]");
+const allClear = document.querySelector("[data-all-clear]");
+const equals = document.querySelector("[data-equals]");
+const smallDisplayTextElement = document.querySelector("[data-small-display]");
+const bigDisplayTextElement = document.querySelector("[data-big-display]");
