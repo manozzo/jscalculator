66704cee0742b8958074ee3d225b6fdbb69e6f4c
Rodrigo Manozzo <rodrigomanozzo@gmail.com>
2021-04-07 07:39:14 -0300

    update js file

---
+++ bank.js
@@ -0,0 +1,4 @@
+if (operation === '+' || operation === '-' || operation === '÷' || operation === '×') && (this.smalldisplay.includes('+') || this.smalldisplay.includes('-') || this.smalldisplay.includes('÷') || this.smalldisplay.includes('×') || ) return;
+if (operation === '+' || operation === '-' || operation === '÷' || operation === '×' && this.smalldisplay.includes('+') || this.smalldisplay.includes('-') || this.smalldisplay.includes('÷') || this.smalldisplay.includes('×') || ) return;
+if ((operation === '+' || operation === '-' || operation === '÷' || operation === '×') && (this.smalldisplay.includes('+') || this.smalldisplay.includes('-') || this.smalldisplay.includes('÷') || this.smalldisplay.includes('×') || )) return;
+
--- script.js
+++ script.js
@@ -1,7 +1,99 @@
+class Calculator {
+  constructor(smallDisplayTextElement, bigDisplayTextElement) {
+    this.smallDisplayTextElement = smallDisplayTextElement;
+    this.bigDisplayTextElement = bigDisplayTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.smalldisplay = "";
+    this.bigdisplay = "";
+    this.operation = undefined;
+  }
+
+  deleteNumbers() {}
+
+  appendNumber(number) {
+    if (number === "." && this.bigdisplay.includes(".")) return;
+    this.bigdisplay = this.bigdisplay.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if (this.bigdisplay === "") return;
+    if (this.smalldisplay !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.smalldisplay = this.bigdisplay;
+    this.smalldisplay = this.bigdisplay.toString() + operation.toString();
+    this.bigdisplay = "";
+  }
+
+  compute() {
+    let computation;
+    const prev = parseFloat(this.smalldisplay);
+    const current = parseFloat(this.bigdisplay);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      case "×":
+        computation = prev * current;
+        break;
+      default:
+        return;
+    }
+    this.bigdisplay = computation;
+    this.operation = undefined;
+    this.smalldisplay = "";
+  }
+
+  updateDisplay() {
+    this.bigDisplayTextElement.innerText = this.bigdisplay;
+    this.smallDisplayTextElement.innerText = this.smalldisplay;
+  }
+}
+
 const numberButtons = document.querySelectorAll("[data-number]");
-const operationButtons = document.querySelectorAll("[data-operations]");
-const delete = document.querySelector("[data-delete]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const deleteButton = document.querySelector("[data-delete]");
 const allClear = document.querySelector("[data-all-clear]");
-const equals = document.querySelector("[data-equals]");
+const equalsButton = document.querySelector("[data-equals]");
 const smallDisplayTextElement = document.querySelector("[data-small-display]");
 const bigDisplayTextElement = document.querySelector("[data-big-display]");
+
+const calculator = new Calculator(
+  smallDisplayTextElement,
+  bigDisplayTextElement
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+allClear.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+equalsButton.addEventListener("click", () => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
